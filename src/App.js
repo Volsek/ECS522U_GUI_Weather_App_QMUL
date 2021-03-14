ae77c0de0cfa43dc40b2793cfaa2cee34a064218
navid-sheik <navidsheikh2020@gmail.com>
2021-03-14 23:51:03 +0000

    Weekly View Implemented

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import WeeklyView from "./components/WeeklyView/WeeklyView";
-
+import HourlyView from "./components/HourlyView/HourlyView";
 function App() {
   return (
     <div className="App">
@@ -12,7 +12,7 @@ function App() {
 
         </div>
         <div id='HourlyView'>
-
+          <HourlyView />
         </div>
         <div id='WeeklyView'>
           <WeeklyView/>
---
+++ src/components/HourlyView/HourlyView.js
@@ -0,0 +1,66 @@
+import "./HourlyView.css"
+import { MDBContainer, MDBRow, MDBCol,MDBIcon } from "mdbreact";
+
+const HourlyView = () => {
+    return (
+        <div className="mainHoursContainer">
+            <h1 class="mb-0">Today</h1>
+            <MDBContainer  fluid={true}  >
+                    <MDBRow id="main_row">
+                        <MDBCol className = "hoursCol" >
+                            <MDBRow center={true} className  = "rowHours">
+                                Hours
+                            </MDBRow>
+                            <MDBRow center={true} className  = "rowPic">
+                                Icon
+                            </MDBRow>
+                            <MDBRow center={true}  className  = "rowTemperature">
+                                Temp
+                            </MDBRow>
+                        </MDBCol>
+
+                        <MDBCol className = "hoursCol">
+                            <MDBRow center={true} className  = "rowHours">
+                                Hours
+                            </MDBRow>
+                            <MDBRow center={true}  className  = "rowPic">
+                                Icon
+                            </MDBRow>
+                            <MDBRow center={true} className  = "rowTemperature">
+                                Temp
+                            </MDBRow>
+                        </MDBCol>
+
+                        <MDBCol className = "hoursCol">
+                            <MDBRow center={true} className  = "rowHours">
+                                Hours
+                            </MDBRow>
+                            <MDBRow center={true}  className  = "rowPic">
+                                Icon
+                            </MDBRow>
+                            <MDBRow center={true} className  = "rowTemperature">
+                                Temp
+                            </MDBRow>
+                        </MDBCol>
+
+                        <MDBCol className = "hoursCol">
+                            <MDBRow center={true} className  = "rowHours">
+                                Hours
+                            </MDBRow>
+                            <MDBRow center={true} className  = "rowPic">
+                                Icon
+                            </MDBRow>
+                            <MDBRow center={true} className  = "rowTemperature">
+                                Temp
+                            </MDBRow>
+                        </MDBCol>
+
+
+
+                    </MDBRow>
+                </MDBContainer>
+        </div>
+    )
+}
+
+export default HourlyView
